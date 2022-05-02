20d28693e19e95fd0465c32f3f7954bbf659653a
SPECTRA995 <maxmillerasce@gmail.com>
2022-05-02 17:25:32 +0300

    feat: update what-season code

--- src/what-season.js
+++ src/what-season.js
@@ -11,29 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- function getSeason(date) {
-   if (date == Date()) {
-     return 'Unable to determine the time of year!'
-   } else {
-     if ((typeof date == 'string')) {
-       return new Error('Invalid date!');
-     } else {
-       let month = date.getMonth();
-       if ( month >= 0 && month <= 1 || month == 11 ) {
-         return "winter";
-       } 
-       if (month >= 2 && month <= 4) {
-         return "spring";
-       } 
-       if (month >= 5 && month <= 7) {
-         return "summer";
-       } 
-       if (month >= 8 && month <= 10) {
-         return "autumn";
-       }
-    }
+
+function isCorrectDate(date) {
+  if(!(date instanceof Date) || Object.getOwnPropertyNames(date).length) {
+    throw new Error ('Invalid date!');
   }
 }
+
+function getSeason(date) {
+  if(!date) {
+    return 'Unable to determine the time of year!';
+  } else {
+    isCorrectDate(date);
+    }
+  
+  let month = date.getMonth();
+  if (month == 11 || month >= 0 && month <= 1) {
+    return "winter";
+  } 
+  if (month >= 2 && month <= 4) {
+    return "spring";
+  }
+  if (month >= 5 && month <= 7) {
+    return "summer";
+  }
+  if (month >= 8 && month <= 10) {
+    return "autumn";
+  }
+}
+
+
+
 
 module.exports = {
   getSeason
